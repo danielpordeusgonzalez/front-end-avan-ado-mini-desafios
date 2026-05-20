51feda99a42d2cdb7e3b66a22edf598ca2d9a7d4
Daniel Pordeus <danielpordeusgonzalez@hotmail.com>
2026-05-20 19:07:55 -0300

    acrescenta novos components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import BotaoAlerta from "./components/BotaoAlerta"
+import CartaoDestaque from "./components/CartaoDestaque"
 import Filme from "./components/Filme"
 import LogoUniesp from "./components/LogoUniesp"
+import MaiorDeIdade from "./components/MaiorDeIdade"
 import MeuPerfil from "./components/MeuPerfil"
 import TrocaTexto from "./components/TrocaTexto"
 
 const App = () => {
-  const filmes = [
-    { id: 1, titulo: "Titanic", ano: 1997 },
-    { id: 2, titulo: "O senhor dos Anéis", ano: 2001 },
-    { id: 3, titulo: "Vingadores", ano: 2012 }
- ]
+//   const filmes = [
+//     { id: 1, titulo: "Titanic", ano: 1997 },
+//     { id: 2, titulo: "O senhor dos Anéis", ano: 2001 },
+//     { id: 3, titulo: "Vingadores", ano: 2012 }
+//  ]
 
   return (
   <div>
-    <MeuPerfil />
+    {/* <MeuPerfil />
     <BotaoAlerta />
     <TrocaTexto/>
     <LogoUniesp/>
@@ -23,6 +25,11 @@ const App = () => {
       <Filme key={filme.id} titulo={filme.titulo} ano={filme.ano}
       />))}
     </ul>
+    <MaiorDeIdade idade={18}/>
+    <MaiorDeIdade idade={21}/>
+    <MaiorDeIdade idade={16}/>
+    <CartaoDestaque /> */}
+    
   </div>
     
   )
